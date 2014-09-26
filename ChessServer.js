73b32429e2844d3f92f35d1cd6643a95f7c9c59e
vegerjiangsir <vegerjiangsir@gmail.com>
2014-09-27 02:31:48 +0800

    rfsd
    afdsfa

---
+++ ChessServer.js
@@ -0,0 +1,103 @@
+var roomID;
+var playerID;
+var ChessServer = {
+    onConnect:function(){
+    },
+    onBroadcast:function(json){
+    },
+    onMessage:function(json){
+    },
+    socketUrl:"http://www.5igame.cc:8080",//服务器地址
+
+    socketConnect:function(){//建立websocket连接
+        socket = io.connect(ChessServer.socketUrl);
+        socket.on('connected',function(){
+            console.log("connected now");
+            ChessServer.onConnect();
+        });
+        socket.on('broadcast', function(json){
+            console.log("broadcast" + JSON.stringify(json));
+            ChessServer.onBroadcast(json);
+        });
+        socket.on('message',function(json){
+            console.log("Message: " + JSON.stringify(json));
+            ChessServer.onMessage(json);
+        });
+        socket.on('createRoomRsp', function(data){
+            console.log("createRoomRsp: " + JSON.stringify(data));
+            roomID = data.roomID;
+            playerID = data.playerID;
+            ChessServer.onCreateRoomRsp(roomID,playerID);
+        });
+        socket.on('joinRoomRsp', function(data){
+            console.log("joinRoomRsp: " + JSON.stringify(data));
+            roomID = data.roomID;
+            playerID = data.playerID;
+            ChessServer.onJoinRoomRsp(roomID,playerID);
+        });
+        socket.on('joinRoomBroadcast', function(data){
+            console.log("joinRoomBraodcast", JSON.stringify(data));
+            ChessServer.onJoinRoomBroadcast(data.roomID,data.peopleNum,data.playerID,data.allPlayerID);
+        });
+        socket.on('leaveRoomBroadcast', function(data){
+            console.log("leaveRoomBraodcast", JSON.stringify(data));
+            ChessServer.onLeaveRoomBroadcast(data.roomID,data.playerID,data.allPlayerID);
+        });
+        socket.on('eventHappenRsp', function(data){
+            console.log("eventHappenRsp", JSON.stringify(data));
+            ChessServer.onEventHappenRsp(data.roomID,data.playerID,data.result);
+        });
+        socket.on('eventHappenBroadcast', function(data){
+            console.log("eventHappenBroadcast", JSON.stringify(data));
+            ChessServer.onEventHappenBroadcast(data.roomID,data.playerID,data.moreInfo);
+        });
+        var finishFunc = function(data){
+            console.log("finish", JSON.stringify(data));
+            ChessServer(data.roomID,data.playerID);
+        };
+        socket.on("finishRoomRsp", finishFunc);
+        socket.on("finishRoomBroadcast", finishFunc);
+    },
+    createRoom: function(peopleNum){
+        console.log("Send createRoom Req");
+        socket.emit("createRoomReq", {peopleNum: peopleNum});
+    },
+    onCreateRoomRsp:function(roomID,playerID){
+        alert("createRoomRsp: "+"roomID="+roomID+", playerID="+playerID);
+    },
+    joinRoom: function(roomID){
+        console.log("Send joinRoom Req");
+        socket.emit("joinRoomReq", {roomID: roomID});
+    },
+    onJoinRoomRsp:function(roomID,playerID){
+        alert("createRoomRsp: "+"roomID="+roomID+", playerID="+playerID);
+    },
+    onJoinRoomBroadcast:function(roomID,peopleNum,playerID,allPlayerID){
+        alert("onJoinRoomBroadcast: roomID="+roomID+", peopleNum="+peopleNum+", playerID="+playerID+", allPlayerID="+allPlayerID);
+    },
+    onLeaveRoomBroadcast:function(roomID,playerID,allPlayerID){
+        alert("onLeaveRoomBroadcast: roomID="+roomID+", playerID="+playerID+", allPlayerID="+allPlayerID);
+    },
+    eventHappen: function(obj){
+        console.log("event happen Req");
+        socket.emit("eventHappenReq", {
+            roomID: roomID,
+            playerID: playerID,
+            moreInfo: obj
+        });
+    },
+    onEventHappenRsp:function(roomID,playerID,result){
+        alert("onEventHappenRsp: roomID="+roomID+", playerID="+playerID+", result="+result);
+    },
+    onEventHappenBroadcast:function(roomID,playerID,moreInfo){
+        alert("onEventHappenBroadcast: roomID="+roomID+", playerID="+playerID+", moreInfo="+moreInfo);
+    },
+    finishRoom: function(roomID){
+        console.log("finish Room Req");
+        socket.emit("finishRoomReq", {roomID: roomID, playerID: playerID});
+    },
+    onFinishRoomRsp:function(roomID,playerID){
+        console.log("onFinishRoomRsp");
+        alert("Winner is " + playerID + " in room: " + roomID);
+    }
+};
